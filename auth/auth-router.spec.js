722b97329ba6673d7a3526592f726db64e591894
Tatyana Dzhura <tatyanadzhura@gmail.com>
2020-07-18 13:44:38 -0400

    MVP met

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -2,6 +2,7 @@ const request = require("supertest")
 const server = require("../api/server")
 const db = require("../database/dbConfig")
 
+
 beforeEach(async () => {
   await db("users").truncate()
 });
