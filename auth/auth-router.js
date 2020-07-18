828dd4fb70d465975e23b19ec2dfcf7bf00e649f
Tatyana Dzhura <tatyanadzhura@gmail.com>
2020-07-18 13:04:52 -0400

    login/registration implemented

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,42 @@
 const router = require('express').Router();
+const Users = require("./auth-model")
+const bcrypt = require("bcryptjs")
+const getToken = require("./getToken")
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res, next) => {
   // implement registration
+  let user = req.body 
+  const hash = bcrypt.hashSync(user.password, 14)
+  user.password = hash
+
+  try{
+    user = await Users.add(user)
+    res.status(201).json(user)
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res, next) => {
   // implement login
+  let { username, password } = req.body 
+
+  try {
+    user = await Users.findBy({ username }).first()
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = getToken(user)
+      res.status(200).json({
+        message: `Welcome ${user.username}!`,
+        token
+      })
+    } else {
+      res.status(401).json({
+        message: "Invalid credentials"
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
 });
 
 module.exports = router;
